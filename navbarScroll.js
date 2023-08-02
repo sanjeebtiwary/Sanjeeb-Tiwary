a3d42c0f3d222e0b0e08222411878399f675c77e
sanjeeb tiwary <63203112+sanjeebtiwary@users.noreply.github.com>
2023-08-02 12:01:40 +0530

    update

--- navbarScroll.js
+++ navbarScroll.js
@@ -27,30 +27,5 @@ setTimeout(() => {
   }, 2000);
 
 
-const slides = document.querySelector('.slides');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-let slideIndex = 0;
-
-// Function to show the current slide
-function showSlide(index) {
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}
-
-// Function to show the previous slide
-function prevSlide() {
-  slideIndex = Math.max(slideIndex - 1, 0);
-  showSlide(slideIndex);
-}
-
-// Function to show the next slide
-function nextSlide() {
-  slideIndex = Math.min(slideIndex + 1, slides.childElementCount - 1);
-  showSlide(slideIndex);
-}
-
-// Event listeners for navigation buttons
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
 
 
